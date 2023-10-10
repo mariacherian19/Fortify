e79d4a5ac3b895ea338a24ff73401bdc301cd8b2
mariacherian19 <maria1cherian@gmail.com>
2023-10-10 11:04:59 +0530

    second commit

--- src/js/Views/resultsView.js
+++ src/js/Views/resultsView.js
@@ -17,7 +17,7 @@ class ResultsView extends View{
            <div class="preview__data">
              <h4 class="preview__title">${res.title}</h4>
              <p class="preview__publisher">${res.publisher}</p>
-             <div class="preview__user-generated">
+             <div class="preview__user-generated ${res.key? '':'hidden'}">
                <svg>
                  <use href="${icons}#icon-user"></use>
                </svg>
--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,4 @@
 export const API_URL='https://forkify-api.herokuapp.com/api/v2/recipes'
 export const TIMEOUT_SEC=10;
 export const RES_PER_APGE=10;
+export const KEY='c134d4db-6b6a-4940-b98b-54bc63e2b6c9';
